93b9dd49c2fcd422343cd2235b92bf5d57bebbf2
Esteban Borai <estebanborai@gmail.com>
2022-06-14 00:21:20 -0400

    feat: build for `iife`

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,31 @@
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import svelte from 'rollup-plugin-svelte';
 
 import pkg from './package.json';
 
+const name = pkg.name
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase());
+
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.module, format: 'es', name: pkg.name, sourcemap: true },
+    { file: pkg.module, format: 'es', name, sourcemap: true },
     {
       file: pkg.main,
       format: 'umd',
-      name: pkg.name,
+      name,
       sourcemap: true,
+    },
+    {
+      file: pkg.main.replace('.js', '.min.js'),
+      format: 'iife',
+      name,
+      plugins: [terser()],
     },
   ],
   plugins: [
